0abc7f7ad28fcf6f3ed8eed8cd9f98a54136179a
Malte <[malte_werner@hotmail.se]>
2026-02-12 14:26:10 +0100

    Moved frans ui api and url to config

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 API_KEY = "special-key"
 BASE_URL = "https://petstore.swagger.io/v2"
+FRANS_API_KEY = "malte"
+FRANS_BASE_URL = "https://test-379574553568.us-central1.run.app"
--- tests/e2e/test_frans_ui.py
+++ tests/e2e/test_frans_ui.py
@@ -1,18 +1,17 @@
 import pytest
 from playwright.sync_api import Page, expect
-
-API_KEY = "malte"
+from config import FRANS_API_KEY, FRANS_BASE_URL
 
 
 def test_frans_ui_has_title(page: Page):
-    page.goto("https://test-379574553568.us-central1.run.app/")
+    page.goto(FRANS_BASE_URL)
     
     expect(page).to_have_title("Frank's API test")
 
 
 def test_create_studen_in_ui(page: Page):
-    page.goto("https://test-379574553568.us-central1.run.app/")
-    page.get_by_placeholder("Enter your API key").fill(API_KEY)
+    page.goto(FRANS_BASE_URL)
+    page.get_by_placeholder("Enter your API key").fill(FRANS_API_KEY)
     page.get_by_placeholder("Enter name").fill("Test")
     page.get_by_placeholder("Enter age").fill("25")
     page.get_by_placeholder("Enter grade").fill("G")
